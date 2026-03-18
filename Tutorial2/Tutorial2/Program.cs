a63cc8117d82c596f13d9d5b1045f821c081d533
Sznyla <dominikszyller@gmail.com>
2026-03-18 22:07:14 +0100

    calculator first operation

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Prosty Kalkulator ");
+
+
+Console.Write("Podaj pierwszą liczbę: ");
+string input1 = Console.ReadLine();
+double num1 = Convert.ToDouble(input1);
+
+
+Console.Write("Wybierz działanie (+, -, *, /): ");
+string op = Console.ReadLine();
+
+
+Console.Write("Podaj drugą liczbę: ");
+string input2 = Console.ReadLine();
+double num2 = Convert.ToDouble(input2);
+
+double result = 0;
+
+
+switch (op)
+{
+    case "+":
+        result = num1 + num2;
+        Console.WriteLine($"Wynik: {num1} + {num2} = {result}");
+        break;
+}
